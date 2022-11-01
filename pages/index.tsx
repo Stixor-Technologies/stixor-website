0b77a3f9786cb1cf8a0b8ed0ed032c252d51ecae
Saba Tanveer <>
2022-11-01 14:37:38 +0500

    add box

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ export default function App() {
         <OrbitControls /> */}
 
         <Box position={[-1.2, 0, 0]} />
-        {/* <Box position={[1.2, 0, 0]} /> */}
+        <Box position={[1.2, 0, 0]} />
       </Canvas>
     </div>
   );
